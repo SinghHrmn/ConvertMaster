06a80f47bbcfa2d5ecf550f021b39de9b84efcf1
SinghHrmn <singh.hrmn98@gmail.com>
2019-06-25 21:26:08 +0530

    csv to Json and xml added

---
+++ main_code/csvtojson.py
@@ -0,0 +1,9 @@
+import csv  
+import json  
+    
+# Open the CSV  
+csvfile = open( 'test_file_Csv.csv', 'r' )  
+reader = csv.DictReader(csvfile)   
+out = json.dumps( [ row for row in reader ],indent=" " )
+jsonfile = open( 'parsed.json', 'w')  
+f.write(out)
