a78e70f6eadb1ac72625666ff05359c85b88b1d2
kritirikhi <kritirikhi1999@gmail.com>
2019-06-30 16:55:10 +0530

    landing page designing started

--- src/convertmaster/urls.py
+++ src/convertmaster/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from accounts import urls as myaccounturls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('convert/',include(myaccounturls),name="blog")
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
