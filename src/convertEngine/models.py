38875f8c9c6c6564d3f50c22f1c9d8f6cca19773
SinghHrmn <singh.hrmn98@gmail.com>
2019-07-12 22:40:20 +0530

    models updated for engine

--- src/convertEngine/models.py
+++ src/convertEngine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class MyConversion(models.Model):
+    methods = [
+        ('xml' , 'XML'),
+        ('json', 'JSON'),
+        ('csv', 'CSV')
+    ]
+    user = models.ForeignKey(User, on_delete='Cascade')
+    original = models.CharField(max_length=4, choices=methods)
+    converted = models.CharField(max_length=4, choices=methods)
+    filename = models.CharField(max_length=20, unique=True)
